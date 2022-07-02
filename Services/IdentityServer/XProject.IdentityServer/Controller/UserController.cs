455940eb7d69b0a428f3cf179a0a1e3e028e9991
Dogan Günes <dogangns.98@gmail.com>
2022-07-03 02:51:40 +0300

    .

---
+++ Services/IdentityServer/XProject.IdentityServer/Controller/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using XProject.Identity.Infrastructure.Dtos;
+using XProject.Identity.Infrastructure.Entities;
+
+namespace XProject.IdentityServer.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignupDto signupDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signupDto.UserName,
+                Email = signupDto.Email,    
+            };
+
+            var result = await _userManager.CreateAsync(user, signupDto.Password);
+            if (!result.Succeeded)
+            {
+                return BadRequest();
+            }
+
+            return Ok(result);
+        }
+    }
+}
