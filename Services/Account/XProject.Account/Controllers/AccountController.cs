eb72994fd23803bf843ed663ecc64ebf59b9fde4
Dogan Günes <dogangns.98@gmail.com>
2022-07-07 00:30:24 +0300

    .

---
+++ Services/Account/XProject.Account/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace XProject.Account.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public AccountController(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+
+
+
+
+    }
+}
