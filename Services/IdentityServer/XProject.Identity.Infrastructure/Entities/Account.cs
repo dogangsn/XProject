797acb070076858234044dcddf123c4cf0a23f8d
Dogan Günes <dogangns.98@gmail.com>
2022-07-09 23:16:55 +0300

    .

---
+++ Services/IdentityServer/XProject.Identity.Infrastructure/Entities/Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using XProject.Shared.Enums;
+
+namespace XProject.Identity.Infrastructure.Entities
+{
+    public class Account
+    {
+        [Key]
+        public string UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public AccountType AccountType { get; set; }
+        public string CompanyId { get; set; }
+        public string RoleId { get; set; }
+        public bool? Passive { get; set; }
+        public bool IsLicenceAccount { get; set; }
+
+    }
+}
--- Services/IdentityServer/XProject.Identity.Infrastructure/Persistence/ApplicationDbContext.cs
+++ Services/IdentityServer/XProject.Identity.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -19,6 +19,8 @@ namespace XProject.Identity.Infrastructure.Persistence
             : base(options)
         {
         }
+        public DbSet<Account> Accounts { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
