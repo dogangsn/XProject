330b3f17f6fceee3cc32d723718b543bb84911dc
Dogan Günes <dogangns.98@gmail.com>
2022-07-11 14:01:23 +0300

    .

---
+++ Services/Account/XProject.Account.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XProject.Account.Domain.Contracts;
+
+namespace XProject.Account.Infrastructure.Repositories
+{
+    public class Repository : IRepository
+    {
+
+    }
+}
