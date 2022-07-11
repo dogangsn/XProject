d545a1b7e7e5b0e648658acee10592a15460d838
Dogan Günes <dogangns.98@gmail.com>
2022-07-12 01:29:25 +0300

    .

---
+++ Shared/XProject.Shared/Accounts/CreateDbDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XProject.Shared.Accounts
+{
+    public class CreateDbDto
+    {
+        public Guid Id { get; set; }
+        public string Company { get; set; }
+
+    }
+}
